a9b638fa0b2a2f4d0d72d054c50e3e9cd9225ec1
Raju Dandigam <raju0574@gmail.com>
2017-12-11 21:40:38 -0500

    Basic alignment and javascript has been updated

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 window.onload = onReady;
 function onReady() {
-  var dynamicTable = document.getElementById('DynamicTable');
-  var rows = dynamicTable.querySelector('#Rows');
-  var columns = dynamicTable.querySelector('#Columns');
-  var rowsCount, columnsCount;
+  var dynamicTable = document.getElementById('DynamicTable'),
+    dynamicTableForm = dynamicTable.querySelector('#DynamicTableForm'),
+    dynamicTableContainer = dynamicTable.querySelector('#TableContainer'),
+    rows = dynamicTable.querySelector('#Rows'),
+    columns = dynamicTable.querySelector('#Columns'),
+    rowsCount, columnsCount;
 
-  dynamicTable.addEventListener('submit', createTable);
+  dynamicTableForm.addEventListener('submit', createTable);
 
   function createTable(event) {
+    var tableElement;
+
     event.preventDefault();
     rowsCount = parseInt(rows.value);
     columnsCount = parseInt(columns.value);
@@ -16,43 +20,41 @@ function onReady() {
       alert("Please enter a number between 1-9");
       return;
     }
-    var tableElement = getTableElement();
-    document.body.appendChild(tableElement);
+    tableElement = getTableElement();
+    dynamicTableContainer.removeChild(dynamicTableContainer.firstChild);
+    dynamicTableContainer.appendChild(tableElement);
   }
 
   function getTableElement() {
-    var table = document.createElement('table');
+    var table = document.createElement('table'),
+        tBody = document.createElement('tbody'),
+        hCellDF = document.createDocumentFragment(),
+        header = table.createTHead(),
+        hRow = header.insertRow(-1),
+        bRow,
+        bCell,
+        headerCell;
 
-    var header = table.createTHead();
- 
-    var row = header.insertRow(-1);
- 
     for (var i = 0; i < columnsCount; i++) {
+        headerCell = document.createElement('th');
      
-        var headerCell = document.createElement('th');
-     
-        headerCell.innerText = "Header"+ i;
-     
-        row.appendChild(headerCell);
-     
+        headerCell.innerText = "Header "+ (i + 1);
+        hCellDF.appendChild(headerCell);
     }
-      var tBody = document.createElement('tbody');
-   
-      table.appendChild(tBody);
 
-      for (var i = 0; i < rowsCount; i++) { // each row
-     
-    row = tBody.insertRow(-1);
-   
-    for (var j = 0; j < columnsCount; j++) { // each column
-     
-      var cell = row.insertCell(-1);
-       
-      cell.innerText = "cell" + i + j;
-   
+    hRow.appendChild(hCellDF);
+    table.appendChild(tBody);
+
+    for (var i = 0; i < rowsCount; i++) { 
+          bRow = tBody.insertRow(-1);
+
+      for (var j = 0; j < columnsCount; j++) {
+          bCell = bRow.insertCell(-1);
+          bCell.innerText = "cell " + (i + 1) + (j + 1);
+      }
+
     }
- 
-}
-return table;
+    return table;
   }
+  
 }
